400126a21eb1fc2e1fd4a0d3330d6e4489211c9c
Charlie Martinez <39219855+charliemartinez@users.noreply.github.com>
2025-12-03 20:30:47 +0100

    Update package URLs in gimp-quirinux-installer.sh

--- gimp-quirinux-installer.sh
+++ gimp-quirinux-installer.sh
@@ -26,17 +26,19 @@ mkdir -p "$DEST"
 
 # Lista de URLs a descargar
 URLS=(
-"https://repo.quirinux.org/pool/main/g/gimp-gap/gimp-gap_2.6.1_amd64.deb"
+"https://repo.quirinux.org/pool/main/g/gimp/libgimp2.0_2.10.34-1+deb12u3_amd64.deb"
 "https://repo.quirinux.org/pool/main/g/gluas/gimp-gluas_0.1.20-2_amd64.deb"
 "https://repo.quirinux.org/pool/main/g/gmic/gimp-gmic_2.9.5-4+b4_amd64.deb"
+"https://repo.quirinux.org/pool/main/g/gutenprint/libgutenprintui2-2_5.3.4.20220624T01008808d602-1_amd64.deb"
 "https://repo.quirinux.org/pool/main/g/gutenprint/gimp-gutenprint_5.3.4.20220624T01008808d602-1_amd64.deb"
 "https://repo.quirinux.org/pool/main/g/gimplensfun/gimp-lensfun_0.2.5-1.1_amd64.deb"
 "https://repo.quirinux.org/pool/main/g/gimp-paint-studio/gimp-paint-studio_2.0-2_all.deb"
 "https://repo.quirinux.org/pool/main/g/gimp-plugin-registry/gimp-plugin-registry_9.20200928+b1_amd64.deb"
-"https://repo.quirinux.org/pool/main/g/gimp-quirinux/gimp-quirinux_6.5.2_all.deb"
 "https://repo.quirinux.org/pool/main/g/gtkam/gtkam-gimp_1.0-3+b1_amd64.deb"
-"https://repo.quirinux.org/pool/main/g/gutenprint/libgutenprintui2-2_5.3.4.20220624T01008808d602-1_amd64.deb"
-"https://repo.quirinux.org/pool/main/g/gimp/libgimp2.0_2.10.34-1+deb12u3_amd64.deb"
+"https://repo.quirinux.org/pool/main/g/gimp-gap/gimp-gap_2.6.1_amd64.deb"
+"https://repo.quirinux.org/pool/main/g/gimp-quirinux/gimp-quirinux_6.5.2_all.deb"
+
+
 )
 
 echo "====> Descargando paquetes .deb necesarios..."
@@ -46,6 +48,7 @@ done
 
 # 3) Instalar los .deb:
 echo "====> Instalando paquetes..."
+apt install mplayer -y
 apt install -y /tmp/gimp-quirinux/*.deb
 
 echo ""
