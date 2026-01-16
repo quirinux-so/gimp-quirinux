64f4123e88c535704d909724d48f9b5597c74790
Charlie Martinez <39219855+charliemartinez@users.noreply.github.com>
2026-01-16 11:43:40 +0100

    Update GIMP Quirinux package version to 6.5.8

--- gimp-quirinux-installer.sh
+++ gimp-quirinux-installer.sh
@@ -68,7 +68,7 @@ URLS=(
 "https://repo.quirinux.org/pool/main/g/gimp-plugin-registry/gimp-plugin-registry_9.20200929+b1_amd64.deb"
 "https://repo.quirinux.org/pool/main/g/gtkam/gtkam-gimp_1.0-3+b1_amd64.deb"
 "https://repo.quirinux.org/pool/main/g/gimp-gap/gimp-gap_2.6.1_amd64.deb"
-"https://repo.quirinux.org/pool/main/g/gimp-quirinux/gimp-quirinux_6.5.6_all.deb"
+"https://repo.quirinux.org/pool/main/g/gimp-quirinux/gimp-quirinux_6.5.8+q2_all.deb"
 )
 
 echo "====> Descargando paquetes Quirinux..."
